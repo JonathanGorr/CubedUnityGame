c175ffcd129a95249ba7a5cfce9e9e508774d051
JonathanGorr <jonathangorr@gmail.com>
2014-04-20 18:49:56 -0400

    added shaders, 'finished' level, added ocean, need well

--- Assets/Scripts/FauxGravity/PlayerController.cs
+++ Assets/Scripts/FauxGravity/PlayerController.cs
@@ -6,18 +6,15 @@ public class PlayerController : MonoBehaviour {
 	public float moveSpeed = 15;
 	public float jumpSpeed = 10;
 	private Vector3 moveDir;
+	//private Vector3 dir;
 
 	void Update () {
 		moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
-
-		//transform.rotation = Quaternion.LookRotation(moveDir);
 	}
 
 	void FixedUpdate() {
 		rigidbody.MovePosition (rigidbody.position + transform.TransformDirection(moveDir) * moveSpeed * Time.deltaTime);
 
-		if (Input.GetKeyDown("W")) {
-						animation.Play ("Running");
-				}
 	}
+
 }
---
+++ Assets/Scripts/ForwardAxisMovementandRotation.js
@@ -0,0 +1,42 @@
+﻿#pragma strict
+
+var speed : float = 0.2;
+var TurnSpeed : float = 2.0;
+
+function LateUpdate () {
+	
+	//if nothing is pressed, stand idle
+	if(Input.anyKey == false)
+	{
+		animation.Play("Stand");
+	}
+	
+	//movement & rotation
+	if(Input.GetKey("w"))
+	{
+		this.transform.position += this.transform.forward * this.speed;
+		animation.Play("Running");
+	}
+	if(Input.GetKey("s"))
+	{
+		this.transform.position -= this.transform.forward * this.speed;
+		animation.Play("Running", PlayMode.StopAll);
+	}
+	if(Input.GetKey("a"))
+	{
+		this.transform.Rotate(0,-this.TurnSpeed,0);
+	}
+	if (Input.GetKey("d"))
+	{
+		this.transform.Rotate(0, this.TurnSpeed, 0);
+	}
+	if (Input.GetKey("space"))
+	{
+		animation.Play("Jump", PlayMode.StopAll);
+	}
+	
+	//animation slowdown
+	for (var state : AnimationState in animation) {
+    state.speed = 0.5;
+} 
+}
