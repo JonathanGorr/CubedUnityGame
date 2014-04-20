cbe21efccdf56c2c9ca14ac195c7e554fc594ac7
JonathanGorr <jonathangorr@gmail.com>
2014-04-20 14:30:53 -0400

    changes

--- Assets/Scripts/FauxGravity/PlayerController.cs
+++ Assets/Scripts/FauxGravity/PlayerController.cs
@@ -4,13 +4,20 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	public float moveSpeed = 15;
+	public float jumpSpeed = 10;
 	private Vector3 moveDir;
 
 	void Update () {
 		moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+
+		//transform.rotation = Quaternion.LookRotation(moveDir);
 	}
 
 	void FixedUpdate() {
 		rigidbody.MovePosition (rigidbody.position + transform.TransformDirection(moveDir) * moveSpeed * Time.deltaTime);
+
+		if (Input.GetKeyDown("W")) {
+						animation.Play ("Running");
+				}
 	}
 }
